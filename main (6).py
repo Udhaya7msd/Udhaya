c6afbec5991402ff9eb24a1a105647472b51c0c2
Udhaya7msd <144436129+Udhaya7msd@users.noreply.github.com>
2023-09-27 14:09:40 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+# Example student objects
+students = [
+    Student("Alice", "A101", 3.8),
+    Student("Bob", "B102", 3.5),
+    Student("Charlie", "C103", 3.9),
+    Student("David", "D104", 3.7),
+]
+
